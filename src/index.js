fd0690f1e1848c7afc4b89f34be5a3abac3b0abb
Fabián Rodríguez <fabianrodriguez85@gmail.com>
2019-11-02 13:29:06 -0500

    Setup hashrouter to allow righ navigation in production mode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 
-ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><HashRouter><App /></HashRouter></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
